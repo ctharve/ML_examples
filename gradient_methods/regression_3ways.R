bb27ab2baeef5790edc864aae5b4c0b17edaebb0
ctharve <ctharve@gmail.com>
2014-08-29 13:11:24 -0400

    add cost function

--- gradient_methods/regression_3ways.R
+++ gradient_methods/regression_3ways.R
@@ -30,11 +30,13 @@ str(x)
 ## for gradient descent, it's a good idea to mean center and scale all of the parameters, 
 ## but first, let's do what we should have done in the first place, which is to plot all of the features.
 ##
-hist(x$cylinders, breaks=10, main='cylinders')  ## discrete [3, 8]
-ggplot(x, aes(x=cylinders)) + geom_density()
-hist(x$horsepower, breaks=100, main='horsepower') ## cont [~48, 200+]
-hist(x$displacement, breaks=100, main='displacement') ## cont [~45, 400+]
-hist(x$weight, breaks=50, main='weight')  ## cont [1500, 5000]
+hist(x[, "cylinders"], breaks=10, main='cylinders')  ## discrete [3, 8]
+hist(x[, "displacement"], breaks=100, main='displacement') ## cont [~45, 400+]
+hist(x[, "horsepower"], breaks=100, main='horsepower') ## cont [~48, 200+]
+hist(x[, "weight"], breaks=50, main='weight')  ## cont [1500, 5000]
+hist(x[, "acceleration"], breaks=50, main='acceleration')  ## cont [~0, 25]
+hist(y, breaks=50, main='mpg')  ## cont [5, 45]
+
 ## xn ~ normalized features
 xn <-  cbind(intercept, Reduce(cbind, lapply(c(2:6), FUN=function(this_feature){
     mu <- mean(x[, this_feature])
@@ -60,42 +62,43 @@ epsilon <- 1E-6   ## convergence threshold
 alpha <- 0.05    ## learning rate
 thetas <- matrix(0, B, n.thetas)   ## initialize matrix for results
 thetas[1, ] <- rep(1, n.thetas)     ## chose zeroes as starting points (may want to change this)
-#thetas[1, ] <- beta
-colnames(thetas) <- colnames(x)   ## theta for each column in data matrix
+cost <- vector(mode='integer', B)   ## monitor the cost/objective function
+colnames(thetas) <- colnames(xn)   ## theta for each column in data matrix
+final_iter <- NA
 
-# replace the original data with normmalized data
-x_original <- x
-x <- xn
-#
-
+##
 for(tt in 2:B){
   #tt <- 2
   prev <- tt-1
   #cat(tt, prev, '\n')
   
   ## will clean this up after testing
-  thetas[tt, 'intercept'] <- thetas[prev, 'intercept'] - alpha*(n.obs^(-1))*( t(x[, 'intercept']) %*% (x %*% (thetas[prev, ]) - y))    
-  thetas[tt, 'cylinders'] <- thetas[prev, 'cylinders'] - alpha*(n.obs^(-1))*( t(x[, 'cylinders']) %*% (x %*% (thetas[prev, ]) - y))    
-  thetas[tt, 'displacement'] <- thetas[prev, 'displacement'] - alpha*(n.obs^(-1))*( t(x[, 'displacement']) %*% (x %*% (thetas[prev, ]) - y))    
-  thetas[tt, 'horsepower'] <- thetas[prev, 'horsepower'] - alpha*(n.obs^(-1))*( t(x[, 'horsepower']) %*% (x %*% (thetas[prev, ]) - y))    
-  thetas[tt, 'weight'] <- thetas[prev, 'weight'] - alpha*(n.obs^(-1))*( t(x[, 'weight']) %*% (x %*% (thetas[prev, ]) - y))    
-  thetas[tt, 'acceleration'] <- thetas[prev, 'acceleration'] - alpha*(n.obs^(-1))*( t(x[, 'acceleration']) %*% (x %*% (thetas[prev, ]) - y))    
-  
+  y_hat <- xn %*% thetas[prev, ]
+  diff <- (y_hat - y)
+  cost[tt] <- n.obs^(-1)*sum(diff^2)
+  thetas[tt, ] <- thetas[prev, ] - alpha*(n.obs^(-1))*( t(xn) %*% diff)
+  #thetas[tt, 'cylinders'] <- thetas[prev, 'cylinders'] - alpha*(n.obs^(-1))*( t(x[, 'cylinders']) %*% (y_hat - y))    
   
   ## reporting
-  ## every hundred iterations print current estimates
-  if(tt%%100 == 0){cat(paste('iteration', tt, sep=' '), '\n', colnames(thetas), '\n', thetas[tt, ], '\n')}
-  #if(tt%%10 == 0){cat(paste('iteration', tt, sep=' '), '\n', theta, '\n')}
-  
+  if(tt%%100 == 0){cat(paste('iteration', tt, sep=' '), '\n', colnames(thetas), '\n', thetas[tt, ], '\n', 'Cost function=', cost[tt], '\n')}
   
   ## convergence
   if(sum( abs(thetas[tt, ]-thetas[prev, ]) <= epsilon) == n.thetas){
+    final_iter <- tt
     cat('Converged at iteration ', tt)
     break
   }
 
 }
-head(thetas); tail(thetas)
+
+thetas[c((final_iter-5):final_iter), ]
+plot(cost[c(2:100)], main='cost')
+plot(thetas[c(2:400), 'intercept'], main='intercept')
+plot(thetas[c(2:400), 'cylinders'], main='cylinders')
+plot(thetas[c(2:400), 'displacement'], main='displacement')
+plot(thetas[c(2:400), 'horsepower'], main='horsepower')
+plot(thetas[c(2:400), 'weight'], main='weight')
+plot(thetas[c(2:400), 'acceleration'], main='acceleration')
 
 ##               ##
 ## ## THE END ## ##
